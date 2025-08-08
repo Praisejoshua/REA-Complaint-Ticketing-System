43aaa013b87410b92aa659ef07b0d81f72600673
Joshua Praise <praizjoshua263@gmail.com>
2025-08-08 16:14:45 +0100

    css wants attention

--- Ticket/settings.py
+++ Ticket/settings.py
@@ -117,8 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"] 
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # where your development static files are stored
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # where Django will collect them for production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
